1fcc4358d047fa96bc52e7ed59327c0ee5cfd685
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2024-06-21 15:59:06 +0100

    added some changes

--- src/app/illustration/Comics.tsx
+++ src/app/illustration/Comics.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Baby from '../../../public/Assets/Baby.png';
-import social from '../../../public/Assets/social.png';
+import Social from '../../../public/Assets/Social.png';
 import anxious from '../../../public/Assets/anxious.png';
 import snuggles from '../../../public/Assets/social.png';
 import flab from '../../../public/Assets/flab.png';
@@ -12,7 +12,7 @@ const Comics = () => {
     <section className='space-y-7 pt-28 lg:p-0 lg:pt-0 md:px-[2rem] w-full overflow-hidden'>
       <div className='flex flex-col md:flex-row md:justify-between'>
         <Image src={Baby} alt='' className='w-[500px] h-auto ' priority />
-        <Image src={social} alt='' className='w-[500px] h-auto' priority />
+        <Image src={Social} alt='' className='w-[500px] h-auto' priority />
       </div>
       <div className='flex justify-between'>
         <Image src={anxious} alt='' className='w-[500px] h-auto ' priority />
