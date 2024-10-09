2192ac2c2f71798613cab8045f17a0c0b5db1eb4
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2024-10-09 02:37:50 +0100

    readded the projects links

--- src/app/quickfill/page.tsx
+++ src/app/quickfill/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Home from '@/components/UI/Quickfills/QuickFill';
-import QuickFill from '@/components/UI/Quickfills/QuickFill';
+import QuickFill from '@/components/UI/Quickfills/Quickfill';
 
 const page = () => {
   return (
